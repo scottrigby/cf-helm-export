7b823f18d6fbd820cbaff97f7865b5e25adc395b
Scott Rigby <scott@r6by.com>
2018-02-22 15:01:10 -0500

    Scale back scope, and make loop work

--- cf-helm-export.sh
+++ cf-helm-export.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
 
+# Validate required args.
+for var in KUBE_CONTEXT SERVICE_NAME; do
+  if [ -z "${!var}" ]; then
+      echo "$var ENV var is required"
+      exit 1
+  fi
+done
+
 kubectl config use-context ${KUBE_CONTEXT}
 
-# @todo Make RELEASE_NAME pattern an ENV-based configuration, to be called from
-#   CF commands along with this script.
-RELEASE_NAME=cf-${CF_REPO_OWNER}-${CF_REPO_NAME}-${CF_PR_NUMBER}
-echo RELEASE_NAME $RELEASE_NAME
+# Defaults.
+PORT=${PORT:-80}
+NAMESPACE=${NAMESPACE:-default}
+TIMEOUT=${TIMEOUT:=60}
+WAIT=${WAIT:-10}
+SCHEME=${SCHEME:-http}
 
-helm install ../codefresh-pr/go-hello/ --namespace=${NAMESPACE} --name ${RELEASE_NAME} --set image.repository=${IMAGE_REPOSITORY} --set image.tag=${IMAGE_TAG}
-
-# @todo Make wait time an ENV-based configuration.
-WAIT=$((SECONDS+30))
-SERVICE_IP=`kubectl get svc --namespace ${NAMESPACE} ${RELEASE_NAME}-go-hello -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-while [ -z "$SERVICE_IP" -o "$SECONDS" -lt "$WAIT" ]
+# Wait for Load Balancer IP to resolve.
+# @todo Support other Service types.
+TIME=$((SECONDS+$TIMEOUT))
+SERVICE_IP=""
+while [ -z "$SERVICE_IP" ] && [ "$SECONDS" -lt "$TIME" ]
 do
-  echo -e "Service IP is still pending. Waiting..."
-  SERVICE_IP=`kubectl get svc --namespace ${NAMESPACE} ${RELEASE_NAME}-go-hello -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-  sleep 5
-done        
+    echo "Service IP is still pending. Waiting..."
+    SERVICE_IP=$(kubectl get svc --namespace ${NAMESPACE} ${SERVICE_NAME} -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    sleep $WAIT
+done
 
-HELM_URL=http://${SERVICE_IP}:${PORT}
-echo "HELM_URL=${HELM_URL}" >> ${CF_VOLUME_PATH}/env_vars_to_export
+# Error if IP doesn't resolve before timout.
+if [ -z "$SERVICE_IP" ]; then
+    echo timed out in $TIME seconds
+    exit 1
+fi
 
-echo HELM_URL ${HELM_URL}
+# If we get this far, export as Codefresh ENV vars.
+echo "SERVICE_IP=${SERVICE_IP}" >> ${CF_VOLUME_PATH}/env_vars_to_export
+echo "Exported SERVICE_IP ${SERVICE_IP}"
+
+echo "HELM_URL=${SCHEME}://${SERVICE_IP}:${PORT}" >> ${CF_VOLUME_PATH}/env_vars_to_export
+echo "Exported HELM_URL ${HELM_URL}"
