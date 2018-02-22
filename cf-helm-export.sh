cde79bf0960ca3758b81ac7a2997d8ce88d3af72
Scott Rigby <scott@r6by.com>
2018-02-22 11:53:34 -0500

    Fix cf_export and add to-dos

--- cf-helm-export.sh
+++ cf-helm-export.sh
@@ -2,11 +2,14 @@
 
 kubectl config use-context ${KUBE_CONTEXT}
 
+# @todo Make RELEASE_NAME pattern an ENV-based configuration, to be called from
+#   CF commands along with this script.
 RELEASE_NAME=cf-${CF_REPO_OWNER}-${CF_REPO_NAME}-${CF_PR_NUMBER}
 echo RELEASE_NAME $RELEASE_NAME
 
 helm install ../codefresh-pr/go-hello/ --namespace=${NAMESPACE} --name ${RELEASE_NAME} --set image.repository=${IMAGE_REPOSITORY} --set image.tag=${IMAGE_TAG}
 
+# @todo Make wait time an ENV-based configuration.
 WAIT=$((SECONDS+30))
 SERVICE_IP=`kubectl get svc --namespace ${NAMESPACE} ${RELEASE_NAME}-go-hello -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
 while [ -z "$SERVICE_IP" -o "$SECONDS" -lt "$WAIT" ]
@@ -16,5 +19,7 @@ do
   sleep 5
 done        
 
-cf_export HELM_URL=$(http://$SERVICE_IP:$PORT)
-echo HELM_URL "http://$SERVICE_IP:$PORT"
+HELM_URL=http://${SERVICE_IP}:${PORT}
+echo "HELM_URL=${HELM_URL}" >> ${CF_VOLUME_PATH}/env_vars_to_export
+
+echo HELM_URL ${HELM_URL}
